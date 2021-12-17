03a89d1f21152cff226171c5c8957fec488998f0
Gera2019 <gumanoid2@gmail.com>
2021-12-17 22:20:27 +0300

    Lesson 3 update. Try to use CamelCaseRenderer by Vitaly Babiy for users only (authapp)

--- todoapp/authapp/serializers.py
+++ todoapp/authapp/serializers.py
@@ -5,4 +5,4 @@ from .models import TodoUser
 class TodoUserModelSerializer(HyperlinkedModelSerializer):
    class Meta:
        model = TodoUser
-       fields = ('username', 'firstname', 'lastname', 'email')
+       fields = ('username', 'first_name', 'last_name', 'email')
--- todoapp/authapp/views.py
+++ todoapp/authapp/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import TodoUser
 from .serializers import TodoUserModelSerializer
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer,CamelCaseBrowsableAPIRenderer
+from djangorestframework_camel_case.parser import CamelCaseJSONParser,CamelCaseFormParser,CamelCaseMultiPartParser
 
 
 class TodoUserModelViewSet(ModelViewSet):
+   renderer_classes = [CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer]
+   parser_classes = [CamelCaseJSONParser, CamelCaseFormParser, CamelCaseMultiPartParser]
    queryset = TodoUser.objects.all()
    serializer_class = TodoUserModelSerializer
